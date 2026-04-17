42c50a901b6710a0761d1cad19aa1ad650e16018
Lezz <leagalejandro@gmail.com>
2026-04-17 03:39:36 -0600

    fix render sync db

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 
 const server = require('./server');
 
+const db = require('./database/models');
+
 const PORT = process.env.PORT || 8080;
+
+db.sequelize.sync({ alter: true });
 
 server.listen(PORT, () => console.log(`Server is live al localhost:${PORT}`));
