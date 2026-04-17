e93d76773aa24ea3e673d454f288e438a62506a6
Lezz <leagalejandro@gmail.com>
2026-04-17 07:32:01 -0600

    Sync

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const db = require('./database/models');
 
 const PORT = process.env.PORT || 8080;
 
-db.sequelize.sync({ alter: true });
+db.sequelize.authenticate()
+  .then(() => console.log("DB conectada"))
+  .then(() => db.sequelize.sync({ alter: true }))
+  .catch(err => console.log(err));
 
 server.listen(PORT, () => console.log(`Server is live al localhost:${PORT}`));
