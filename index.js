a8873489e11fad2b16042e82b852b52dd8b9bc52
Lezz <leagalejandro@gmail.com>
2026-04-17 07:38:32 -0600

    fix db sync render properly

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 require('dotenv').config();
 
 const server = require('./server');
-
 const db = require('./database/models');
 
 const PORT = process.env.PORT || 8080;
 
 db.sequelize.authenticate()
-  .then(() => console.log("DB conectada"))
-  .then(() => db.sequelize.sync({ alter: true }))
-  .catch(err => console.log(err));
+  .then(() => {
+    console.log("DB conectada");
 
-server.listen(PORT, () => console.log(`Server is live al localhost:${PORT}`));
+    return db.sequelize.sync({ alter: true });
+  })
+  .then(() => {
+    console.log("Tablas creadas / sincronizadas");
+  })
+  .catch(err => {
+    console.error("ERROR DB:", err);
+  });
+
+server.listen(PORT, () =>
+  console.log(`Server is live al localhost:${PORT}`)
+);
