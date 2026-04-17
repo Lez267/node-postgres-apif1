25308a73dc6e5170dd895ce1340e394777ae695f
Lezz <leagalejandro@gmail.com>
2026-04-17 09:34:18 -0600

    fix

--- database/config/config.js
+++ database/config/config.js
@@ -5,12 +5,20 @@ module.exports = {
     url: process.env.DEV_DATABASE_URL,
     dialect: 'postgres',
   },
+
   test: {
     url: process.env.TEST_DATABASE_URL,
     dialect: 'postgres',
   },
+
   production: {
     url: process.env.DATABASE_URL,
     dialect: 'postgres',
-  },
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
 }
--- index.js
+++ index.js
@@ -7,17 +7,12 @@ const PORT = process.env.PORT || 8080;
 
 db.sequelize.authenticate()
   .then(() => {
-    console.log("DB conectada");
-
+    console.log("DB conectada correctamente");
     return db.sequelize.sync({ alter: true });
   })
-  .then(() => {
-    console.log("Tablas creadas / sincronizadas");
-  })
-  .catch(err => {
-    console.error("ERROR DB:", err);
-  });
+  .then(() => console.log("Tablas sincronizadas"))
+  .catch(err => console.error("Error de conexión:", err));
 
 server.listen(PORT, () =>
-  console.log(`Server is live al localhost:${PORT}`)
+  console.log(`Server is live at localhost:${PORT}`)
 );
